81ff9908070acb7fb2f44bd388588db3489054fe
sanaz <sanazmarand@gmail.com>
2020-01-17 13:57:58 -0600

    methodexercise

--- MethodExercise/MethodExercise/Program.cs
+++ MethodExercise/MethodExercise/Program.cs
@@ -29,9 +29,73 @@ namespace MethodExercise
 
             Console.WriteLine($"My favorite sport is {sportType}");
 
+            
+
+          Console.WriteLine( Sum(8, 16));
+          Console.WriteLine( Sum(5, 7) );
+
+            Console.WriteLine(Mul(10, 2));
+            Console.WriteLine(Mul(35, 6));
+
+            Console.WriteLine(Sum(2, 4, 6));
+
+            Add(1, 300);
+            Add(3, 900);
+            Add(3, 5);
+            Add(3.6, 5.6);
+            Add(20, 4, true);
+           
 
 
             
         }
+        public static int Sum(int first, int second)
+        {
+            return first + second;
+        }
+        public static int Mul(int first, int second)
+        {
+            return first * second;
+        }
+        public static int Sum(int first, int second, int third)
+        {
+            return first + second + third;
+        }
+        public static void Add(int firstNum, int secondNum)
+        {
+            Console.WriteLine(firstNum + secondNum);
+        }
+        public static void Add(double firstNum, double secondNum)
+        {
+            Console.WriteLine(firstNum + secondNum);
+        }
+        public static void Add(int x, int y, int z)
+        {
+            Console.WriteLine(x + y + z);
+        }
+        public static decimal Add(decimal a, decimal b)
+        {
+            return (a + b);
+        }
+        public static string Add(int a, int b, bool istrue)
+        {
+            var answer = a + b;
+          if(istrue==true)
+            {
+                return $"{a + b} dollars";
+
+
+
+
+            }
+          else
+            {
+                return $"{a + b} dollar";
+            }
+        }
     }
+
+   
+
+       
 }
